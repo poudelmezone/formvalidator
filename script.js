12089403641325cfff96093b0c93bf64c1afa811
MezonePoudel <145553532+poudelmezone@users.noreply.github.com>
2024-02-15 13:01:15 +0545

    added all files

---
+++ script.js
@@ -0,0 +1,77 @@
+var nameError = document.getElementById("name-error");
+var phoneError = document.getElementById("phone-error");
+var emailError = document.getElementById("email-error");
+var messageError = document.getElementById("message-error");
+var submitError = document.getElementById("submit-error");
+
+function validateName(){
+    var name = document.getElementById("contact-name").value;
+
+    if(name.length == 0){
+        nameError.innerHTML = "Name is required";
+        return false;
+    }
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = "Name is Invalid";
+        return false;
+    }
+    nameError.innerHTML = '<i class="fas fa-check-circle"></i>';
+    return true;
+}
+
+function validatePhone(){
+    var phone = document.getElementById("contact-phone").value;
+
+    if(phone.length == 0){
+        phoneError.innerHTML = "Phone No. is required";
+        return false;
+    }
+    if(phone.length !== 10){
+        phoneError.innerHTML = "Enter 10 digit phone No.";
+        return false;
+    }
+    if(!phone.match(/^[0-9]{10}$/)){
+        phoneError.innerHTML = "Only digits";
+        return false;
+    }
+    phoneError.innerHTML = '<i class="fas fa-check-circle"></i>';
+    return true;
+}
+
+function validateEmail(){
+    var email = document.getElementById("contact-email").value;
+
+    if(email.length == 0){
+        emailError.innerHTML = "Email is required";
+        return false;
+    }
+    if(!email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
+        emailError.innerHTML = "Email Invalid";
+        return false;
+    }
+    emailError.innerHTML = '<i class="fas fa-check-circle"></i>';
+    return true;
+}
+
+function validateMessage(){
+    var message = document.getElementById("contact-message").value;
+    var required = 20;
+    var left = required - message.length;
+
+    if(left>0){
+        messageError.innerHTML = left + ' more characters required';
+        return false;
+    }
+
+    messageError.innerHTML = '<i class="fas fa-check-circle"></i>'
+    return true;
+}
+
+function validateForm(){
+    if(!validateName() ||validateEmail()||validatePhone()||validateMessage()){
+        submitError.style.display = "block";
+        submitError.innerHTML = "Please fix all the Errors to submit";
+        setTimeout(function (){submitError.style.display = "none";},3000)
+        return false;
+    }
+}
